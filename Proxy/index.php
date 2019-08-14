f903be44763ae35dfd1f202b732c6676e99c8d2e
JVFayad <joao.victor.fa2014@gmail.com>
2019-08-13 21:18:21 -0300

    index Proxy

--- Proxy/index.php
+++ Proxy/index.php
@@ -1,5 +1,17 @@
 <?php
 
+require_once('ReceitaProxy.php');
 
+echo "Usuario Comum vai pagar\n";
+
+$receitaSession1 = new ReceitaProxy('usuario', '4321');
+$receitaSession1->pagarFisico();
+$receitaSession1->pagarJuridico();
+
+echo "\nAdmin vai pagar\n";
+
+$receitaSession2 = new ReceitaProxy('admin', '1234');
+$receitaSession2->pagarFisico();
+$receitaSession2->pagarJuridico();
 
 ?>
